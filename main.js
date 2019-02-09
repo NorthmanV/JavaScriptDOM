d7ea03c32891e9c7040260794961e008bb2b3adb
Ruslan Akberov <ruslanakberov@gmail.com>
2019-02-09 14:22:31 +0300

    Removing and filtering elements implemnted

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var form = document.getElementById("addForm")
 var itemList = document.getElementById("items")
+var filter = document.getElementById("filter")
 
+// Add item
 form.addEventListener("submit", addItem)
 
 function addItem(event) {
@@ -17,3 +19,32 @@ function addItem(event) {
 
     itemList.appendChild(li)
 }
+
+// Remove item
+itemList.addEventListener("click", removeItem)
+
+function removeItem(event) {
+    if(event.target.classList.contains("delete")) {
+        if(confirm("Are you sure?")) {
+            console.log(event.target)
+            var li = event.target.parentElement
+            itemList.removeChild(li)
+        }
+    }
+}
+
+// Filter items
+filter.addEventListener("keyup", filterItems)
+function filterItems(event) {
+    var text = event.target.value.toLowerCase()
+    var items = itemList.getElementsByTagName("li")
+    Array.from(items).forEach(function(item) {
+        var itemName = item.firstChild.textContent
+        if(itemName.toLowerCase().includes(text)) {
+            item.style.display = "block"
+        } else {
+            item.style.display = "none"
+        }
+    })
+}
+
